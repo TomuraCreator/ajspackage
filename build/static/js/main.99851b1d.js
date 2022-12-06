8e2044a902a44a2fbd23462331b3f1adb1ef921e
Azathoth <vladislavutkin92@gmail.com>
2022-12-06 16:13:34 +0400

    dsds

---
+++ build/static/js/main.99851b1d.js
@@ -0,0 +1,2 @@
+!function(){"use strict";var e={};e.d=function(t,r){for(var n in r)e.o(r,n)&&!e.o(t,n)&&Object.defineProperty(t,n,{enumerable:!0,get:r[n]})},e.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)}}();
+//# sourceMappingURL=main.99851b1d.js.map
